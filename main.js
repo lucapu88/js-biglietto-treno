fcdc799227e953fd20af1312cd592f13e684ad7f
lucapu88 <caputoluca88@gmail.com>
2019-11-14 17:25:47 +0100

    mi da NaN se inserisco le scontistiche

--- main.js
+++ main.js
@@ -3,16 +3,16 @@ console.log(chilometri);
 var età = parseInt(prompt('Grazie! Ora dicci la tua età così calcolo la scontistica'));
 console.log(età);
 //calcolo del costo del Biglietto
-var biglietto =  Math.ceil(chilometri * 0.21);
+var biglietto =  Math.ceil(chilometri * 0.21) + '€';
 
 //calcolo dello sconto
 if (età < 18) { // se il passeggero ha meno di 18 anni
-  var sconto1 = (biglietto * 20) / 100;
+  var sconto1 = ((biglietto * 20) / 100) + '€';
   document.getElementById('costo').innerHTML = sconto1;
   console.log(sconto1);
 }
 else if (età > 65 ) { // se il passeggero ha più di 65 anni
-  var sconto2 = (biglietto * 40) / 100;
+  var sconto2 = ((biglietto * 40) / 100) + '€';
   document.getElementById('costo').innerHTML = sconto2;
   console.log(sconto2);
 }
